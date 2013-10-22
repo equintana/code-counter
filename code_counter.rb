9bcf832cf4645fde0b665cf969be8f9843a65f76
Erlinis Quintana <ing.erlinisquintana@gmail.com>
2013-10-22 17:14:02 -0500

    Removing some methods to optimize the script

--- code_counter.rb
+++ code_counter.rb
@@ -1,53 +1,37 @@
 # code_counter.rb
 =begin
-   Read the file that you pass by as first argument
-   in the terminal and tell you how many line of code have that file.
+   Read the file that you pass by as first argument in the terminal and tell you how many line of code have that file.
    
    Ignore: 
    1. In-line comments. Every line start with #.
-   2. Empty line
-   3. Block comments. Everything between =begin and =end 
+   2. Empty lines.
+   3. Block comments. Everything between =begin and =end.
 =end
 
-def is_a_in_line_comment(line)
-  return line.start_with?("#")  
-end
+IN_LINE_COMMENT = "#"
+START_BLOCK_COMMENT = "=begin"
+END_BLOCK_COMMENT = "=end"
 
-def is_a_empty_line(line)
-  return line.empty?
-end
-
-def is_line_of_code(line)
-  if is_a_in_line_comment(line)
-    return false
-  end
-  if is_a_empty_line(line)
+def is_line_of_code( line )
+  if line.empty? || line.start_with?( IN_LINE_COMMENT )
     return false
   end
   return true
 end
 
-def is_start_block_comment(line)
-  return line.start_with?("=begin")  
-end
-
-def is_fin_block_comment(line)
-  return line.start_with?("=end")  
-end
-
-def count_lines(file_entered, print_lines_of_code)
+def count_lines( file_entered, print_lines_of_code )
   file_to_read = file_entered ? file_entered : $0
   
   lines_of_code = 0
   block_comment_started = false
 
-  File.open(file_to_read, 'r') do |f1|  
-    while line = f1.gets  
+  File.open(file_to_read, 'r') do |file|  
+    while line = file.gets  
       if block_comment_started
-          if is_fin_block_comment(line)
+          if line.start_with?( END_BLOCK_COMMENT )
             block_comment_started = false
           end
-      elsif is_start_block_comment(line)
+      elsif line.start_with?( START_BLOCK_COMMENT )
           block_comment_started = true
       elsif is_line_of_code( line.lstrip  )
         lines_of_code = lines_of_code + 1
@@ -57,14 +41,10 @@ def count_lines(file_entered, print_lines_of_code)
       end
     end
   end
-  return lines_of_code
+  lines_of_code
 end
 
 filename = ARGV.first
-puts "You entered %s" % (filename ? filename : "nothing, so I will read my own file")
-
-print_lines_of_code = false
-
-
-lines_of_code = count_lines(filename, print_lines_of_code)
+puts "You entered %s" % ( filename ? filename : "nothing, so I will read my own file" )
+lines_of_code = count_lines(filename, false)
 puts '%i LOC' % lines_of_code
