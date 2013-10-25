2356431424608e05801abf6b610712eeebdf69b2
Erlinis Quintana <ing.erlinisquintana@gmail.com>
2013-10-25 09:42:47 -0500

    calculation using arrays of x and y instead of sampless arrays

--- stats.rb
+++ stats.rb
@@ -1,8 +1,11 @@
 #86 LOC using Enumerable
 require 'csv'
+
 class Sample
 
   attr_accessor :x, :y
+  attr_reader :x, :y
+
   def initialize(x,y)
     @x, @y = x, y
   end
@@ -22,64 +25,50 @@ class Stats
     @round = round
   end
 
-  def calculate_b1(samples)
-    n = samples.size
-
-    data_x = samples.collect{ |i| i.x }
-    data_y = samples.collect{ |i| i.y }
+ def calculate_b1(data_x, data_y)
+    n = data_x.count
     average_x = average( data_x )
     average_y = average( data_y )
 
-    part_1 = summation_x_multiplied_y( samples )
-    part_2 = n * average_x * average_y
-    part_3 = summation_power_of_two( data_x )
-    part_4 = n * ( average_x * average_x )
+    quotient = summation_x_multiplied_y( data_x, data_y ) - ( n * average_x * average_y )
+    divider = summation_power_of_two( data_x ) - ( n * ( average_x * average_x ) )
 
-    b1 = (part_1 - part_2) / (part_3 - part_4)
+    b1 = (quotient / divider)
     b1.round(@round)
   end
 
-  def calculate_b0(b1, samples)
-    average_x = average( samples.collect{ |i| i.x} )
-    average_y = average( samples.collect{ |i| i.y} )
-
-    b0 = average_y - (b1 * average_x)
+  def calculate_b0(b1, data_x, data_y)
+    b0 = average( data_y ) - ( b1 * average( data_x ) )
     b0.round(@round)
   end
 
-  def calculate_r(samples)
-    n = samples.size
-
-    data_x = samples.collect{ |i| i.x }
-    data_y = samples.collect{ |i| i.y }
+  def calculate_r(data_x, data_y)
+     n = data_x.size
     summation_x = data_x.reduce(:+)
     summation_y = data_y.reduce(:+)
 
-    part_1 = n * summation_x_multiplied_y( samples )
-    part_2 = summation_x * summation_y
-    part_3 = ( n * summation_power_of_two( data_x ) ) - ( summation_x**2 )
-    part_4 = ( n * summation_power_of_two( data_y ) ) - ( summation_y**2 )
+    quotient = n * summation_x_multiplied_y( data_x, data_y ) - ( summation_x * summation_y )
+    divider_1 = ( n * summation_power_of_two( data_x ) ) - ( summation_x**2 )
+    divider_2 = ( n * summation_power_of_two( data_y ) ) - ( summation_y**2 )
 
-    r = (part_1 - part_2) / Math.sqrt( part_3 * part_4)
+    r = quotient / Math.sqrt( divider_1 * divider_2)
     r.round(@round)
   end
 
   def calcuate_r_power_two(r)
-    r2 = r * r
-    r2.round(@round)
+    r2 = (r * r).round(@round)
   end
 
   def calculate_prediction(b0, b1)
-    p = b0 + b1 * E
-    p.round(@round)
+    p = (b0 + b1 * E).round(@round)
   end
 
   def average(data)
     data.reduce(:+) / data.count
   end
 
-  def summation_x_multiplied_y(samples)
-    samples.inject(0){ |sum, sample| (sample.x * sample.y) + sum }
+  def summation_x_multiplied_y(data_x, data_y)
+    (0...data_x.count).inject(0){ |sum_xy, i| sum_xy + data_x[i]*data_y[i] }
   end
 
   def summation_power_of_two(data)
@@ -97,14 +86,15 @@ end
 
 filename = ARGV.first 
 samples = extract_data_csv( filename )
+data_x = samples.collect{ |i| i.x }
+data_y = samples.collect{ |i| i.y }
 
 stats = Stats.new(4)
-b1 = stats.calculate_b1(samples)
-b0 = stats.calculate_b0(b1, samples)
-r = stats.calculate_r(samples)
+b1 = stats.calculate_b1(data_x, data_y)
+b0 = stats.calculate_b0(b1, data_x, data_y)
+r = stats.calculate_r(data_x, data_y)
 r2 = stats.calcuate_r_power_two(r)
 p = stats.calculate_prediction(b0, b1)
-
 puts "Test #{filename}> b0: #{b0}, b1: #{b1}, r: #{r}, r2: #{r2}, p:#{p}"
 
 
